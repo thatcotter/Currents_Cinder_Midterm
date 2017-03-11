af7b82fa8aa223fab6f3eb0bf3d52da568bf5796
Andrew Cotter <cotter.andrew.p@gmail.com>
2017-03-11 18:39:16 -0500

    merge

--- Pong_OSC_Host/xcode/build/Pong_OSC_Host.build/Debug/Pong_OSC_Host.build/Objects-normal/x86_64/Puck.d
+++ Pong_OSC_Host/xcode/build/Pong_OSC_Host.build/Debug/Pong_OSC_Host.build/Objects-normal/x86_64/Puck.d
@@ -1,4 +1,4 @@
 dependencies: \
-  /Users/andrewcotter/Documents/Code/cinder_0.9.0_mac/Currents_Cinder_Midterm/Pong_OSC_Host/src/Puck.cpp \
-  /Users/andrewcotter/Documents/Code/cinder_0.9.0_mac/Currents_Cinder_Midterm/Pong_OSC_Host/src/Puck.h \
+  /Users/kimkohler/Desktop/cinder_0.9.0_mac/Currents_Cinder_Midterm/Pong_OSC_Host/src/Puck.cpp \
+  /Users/kimkohler/Desktop/cinder_0.9.0_mac/Currents_Cinder_Midterm/Pong_OSC_Host/src/Puck.h \
   ../../../include/cinder/Rand.h
