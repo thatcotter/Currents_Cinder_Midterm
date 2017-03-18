3dd297265cd27597c35d93818bed2dfc5532abf1
Andrew Cotter <cotter.andrew.p@gmail.com>
2017-03-17 20:48:58 -0400

    sending score

--- Pong_OSC_Host/xcode/build/Pong_OSC_Host.build/Debug/Pong_OSC_Host.build/Objects-normal/x86_64/NetworkingUtils.d
+++ Pong_OSC_Host/xcode/build/Pong_OSC_Host.build/Debug/Pong_OSC_Host.build/Objects-normal/x86_64/NetworkingUtils.d
@@ -1,3 +1,3 @@
 dependencies: \
-  /Users/kimkohler/Desktop/cinder_0.9.0_mac/Currents_Cinder_Midterm/Pong_OSC_Host/blocks/OSC/src/ip/posix/NetworkingUtils.cpp \
+  /Users/andrewcotter/Documents/Code/cinder_0.9.0_mac/Currents_Cinder_Midterm/Pong_OSC_Host/blocks/OSC/src/ip/posix/NetworkingUtils.cpp \
   ../blocks/OSC/src/ip/NetworkingUtils.h
